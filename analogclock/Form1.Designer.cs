420ba984d5c5569fe65a6e9064d5b1dc2c349059
こまめ <54506591+Komame3@users.noreply.github.com>
2021-11-16 01:51:05 +0900

    完成

--- analogclock/Form1.Designer.cs
+++ analogclock/Form1.Designer.cs
@@ -32,51 +32,30 @@ namespace analogclock
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(103, 75);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(99, 248);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(112, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(26, 190);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(136, 190);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(327, 297);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -90,8 +69,6 @@ namespace analogclock
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- analogclock/Form1.cs
+++ analogclock/Form1.cs
@@ -23,13 +23,48 @@ namespace analogclock
         {
             DateTime d = DateTime.Now;
             label1.Text = d.ToLongTimeString();
+
+            //画面を消す
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.Black);
+            g.Dispose();
+
+            //針を書く
+            draw(6, Pens.Aqua, d.Second, 100); //秒針 6→360÷60
+            draw(6, Pens.Yellow, d.Minute, 80);  //分 
+            draw(30, Pens.Pink, d.Hour, 50);    //時  15→360÷12(時間) 短針は一日に二周するため2倍
+        }
+        
+        private void draw(int 倍率, Pen pen, int time, int 針の長さ)
+        {
+            int cx = 150;
+            int cy = 150;
+
+            //原点
+            int x1 = cx;
+            int y1 = cy;
+
+            //針
+            double 角度1度あたり = Math.PI / 180;
+            double 角度 = 角度1度あたり * 倍率 * time - Math.PI / 2;
+
+            //int 針の長さ = 100;
+            int x2 = cx + (int)(Math.Cos(角度) * 針の長さ); //引数はラジアンで渡す必要がある
+            int y2 = cy + (int)(Math.Sin(角度) * 針の長さ);
+
+            Graphics g = this.CreateGraphics();
+            //g.Clear(Color.Black);
+            g.DrawLine(pen , x1, y1, x2, y2);
+            g.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             Graphics g = this.CreateGraphics(); //線を書くことができる
             g.DrawLine(Pens.Aqua, 0, 0, 100, 200); //(Pens.色. 最初の座標X,Y,目的の座標X,Y)
             g.Dispose(); //リソースの開放 C#にはusingというのもある
+            */
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -41,6 +76,11 @@ namespace analogclock
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
